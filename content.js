8dcc36b8ff5fd43cb38bcc5cc22264061ace2aa3
TimFinnigan <tim.finnigan1@gmail.com>
2023-04-04 21:31:26 -0700

    add merge functionality

--- content.js
+++ content.js
@@ -1,7 +1,28 @@
 chrome.runtime.sendMessage({
 	action: 'screenDimensions',
-	dimensions: {
-		width: window.screen.availWidth,
-		height: window.screen.availHeight,
-	},
+	dimensions: { width: window.innerWidth, height: window.innerHeight },
+});
+
+document.addEventListener('keydown', (event) => {
+	if (
+		(event.metaKey || event.ctrlKey) &&
+		event.shiftKey &&
+		event.key === 'U'
+	) {
+		chrome.runtime.sendMessage({ action: 'show_tab_breakout' });
+	}
+});
+
+document.addEventListener('click', () => {
+	chrome.storage.local.get(
+		['isOriginalWindow', 'originalWindowId'],
+		(data) => {
+			if (!data.isOriginalWindow && data.originalWindowId) {
+				chrome.runtime.sendMessage({
+					action: 'windowClicked',
+					originalWindowId: data.originalWindowId,
+				});
+			}
+		}
+	);
 });
--- service-worker.js
+++ service-worker.js
@@ -3,15 +3,10 @@ let screenDimensions = { width: 0, height: 0 };
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	if (request.action === 'screenDimensions') {
 		screenDimensions = request.dimensions;
-	}
-});
-
-chrome.commands.onCommand.addListener((command) => {
-	if (command === 'show_tab_breakout') {
+	} else if (request.action === 'show_tab_breakout') {
 		chrome.tabs.query({ currentWindow: true }, (tabs) => {
 			const screenWidth = screenDimensions.width;
 			const screenHeight = screenDimensions.height;
-
 			const windowWidth = Math.floor(
 				screenWidth / Math.ceil(Math.sqrt(tabs.length))
 			);
@@ -23,14 +18,25 @@ chrome.commands.onCommand.addListener((command) => {
 			let yPos = 0;
 
 			tabs.forEach((tab, index) => {
-				chrome.windows.create({
-					tabId: tab.id,
-					left: xPos,
-					top: yPos,
-					width: windowWidth,
-					height: windowHeight,
-					focused: index === 0,
-				});
+				const tabId = tab.id;
+				chrome.windows.create(
+					{
+						tabId: tabId,
+						left: xPos,
+						top: yPos,
+						width: windowWidth,
+						height: windowHeight,
+						focused: index === 0,
+					},
+					(newWindow) => {
+						chrome.storage.sync.set({ isOriginalWindow: false });
+						chrome.windows.getCurrent((window) => {
+							chrome.storage.local.set({
+								originalWindowId: window.id,
+							});
+						});
+					}
+				);
 
 				xPos += windowWidth;
 				if (xPos >= screenWidth) {
@@ -39,5 +45,30 @@ chrome.commands.onCommand.addListener((command) => {
 				}
 			});
 		});
+	} else if (request.action === 'windowClicked') {
+		if (request.originalWindowId) {
+			mergeAllWindows(request.originalWindowId);
+		}
 	}
 });
+
+function mergeAllWindows(originalWindowId) {
+	chrome.windows.getAll({ populate: true }, (windows) => {
+		const originalWindow = windows.find((w) => w.id === originalWindowId);
+		if (!originalWindow) return;
+
+		windows.forEach((window) => {
+			if (window.id === originalWindowId) return;
+
+			window.tabs.forEach((tab) => {
+				chrome.tabs.move(tab.id, {
+					windowId: originalWindow.id,
+					index: -1,
+				});
+			});
+			chrome.windows.remove(window.id);
+		});
+
+		chrome.windows.update(originalWindow.id, { focused: true });
+	});
+}
